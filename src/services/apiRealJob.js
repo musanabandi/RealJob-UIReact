f7619f71b2ebbb5dd6d9da08f221636cdf7b8805
mediatrice <meddyu15@gmail.com>
2021-06-02 09:32:37 +0200

    This is Dashboard where user can make application and if it  is needed can upload pic and upload cv after submit the application

--- src/services/apiRealJob.js
+++ src/services/apiRealJob.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {notification} from 'antd';
 
-const RealJobUrl = 'https://blogpost-shecancode-api.herokuapp.com/api/v1/blo' ;
+const RealJobUrl = '' ;
 
 
 class RealJobApi{
---
+++ src/views/JobSeeker.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import MainLayout from '../components/MainLayout';
+import JobSeekerDashboard from '../components/JobSeekerDashboard';
+
+
+const JobSeeker =()=> {
+    return(
+
+
+       <MainLayout>
+
+ <JobSeekerDashboard></JobSeekerDashboard>
+ 
+       </MainLayout>
+    )
+   
+}
+
+export  default  JobSeeker;
