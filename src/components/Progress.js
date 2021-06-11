7b5a335e7b3d63bf5db83f0935d1c5b1446008c6
SOLEIL <iraduclariss51@gmail.com>
2021-06-11 14:12:21 +0200

    progress activities

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import './index.css';
-import { Progress, Card } from 'antd';
+import { Progress, Card,Button } from 'antd';
+import { MinusOutlined, PlusOutlined } from '@ant-design/icons';
+
 const ProgressComponent = () => {
+
+    
+
+
 return(
-  <Card>
-    <Progress percent={30} />
-    <Progress percent={50} status="active" />
-    <Progress percent={70} status="exception" />
-    <Progress percent={100} />
-    <Progress percent={50} showInfo={false} />
-  </Card>
+  <Card><div> <h1 className="h1">this is your statistics As JobSeeker</h1>
+      Your All Applications&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<Progress type="circle" percent={100} /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    Cancelled Applications&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<Progress type="circle" percent={70} status="exception" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+Succesfully Applications&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<Progress type="circle" percent={75} />
+
+</div>
+<div>
+<h1>this is your statistics As JobProvider</h1>
+      
+<Progress percent={30} size="small" />
+    all applicants on Software Developers jobs<Progress percent={50} size="small" status="active" />
+    Current Active Jobs<Progress percent={70} size="small" status="exception" />
+    All posted Jobs<Progress percent={100} size="small" />
+
+</div>
+</Card>
+
   );
   };
+
   export default ProgressComponent;
