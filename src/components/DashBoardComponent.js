9f81f46783d4ad06f93e51c241057f5ce9842563
UwitonzeMediatrice <80101019+musanabandi@users.noreply.github.com>
2021-06-18 09:26:37 +0200

    Update DashBoardComponent.js

--- src/components/DashBoardComponent.js
+++ src/components/DashBoardComponent.js
@@ -13,7 +13,7 @@ return(
   <Card style={{ minHeight:'100vh', width:'100%', marginLeft:'280px'}} >
     
     <div style={{marginTop:'120px', width:'100px', height:'100%'}}>
-    <h1 style={{fontSize:'25px', width:'500px'}} >Here We  Statistics </h1>
+    <h1 style={{fontSize:'25px', width:'500px'}} >Here We Have Statistics </h1>
 
       Your All Applications
       <Progress type="circle" percent={100} style={{marginLeft:'50px'}}/> 
